9c7b48db2531979ad52e0f01b0710a1e5dfd7d78
taminhtri20 <taminhtri20@gmail.com>
2024-05-14 08:30:04 +0700

    commit

---
+++ src/main/java/com/example/quanlydaotao/model/InternProfile.java
@@ -0,0 +1,26 @@
+package com.example.quanlydaotao.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity // dùng để khai báo với Spring Boot rằng đây là 1 entity biểu diễn table trong db
+@Data // annotation này sẽ tự động khai báo getter và setter cho class
+@AllArgsConstructor // dùng để khai báo constructor với tất cả các properties
+@NoArgsConstructor
+@Table(name = "intern_profiles")
+public class InternProfile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String trainingState;
+    private boolean isPass;
+}
