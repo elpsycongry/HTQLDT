62bab0aeb7bc4f4ca74516e66f748c416377a82d
Nguyenhoang164 <kamito062004@gmail.com>
2024-05-15 14:16:12 +0700

    save

--- src/main/java/com/example/quanlydaotao/service/impl/RecruitmentPlanService.java
+++ src/main/java/com/example/quanlydaotao/service/impl/RecruitmentPlanService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -83,7 +84,15 @@ public class RecruitmentPlanService implements IRecruitmentPlanService {
 
         recruitmentPlan.setUsers(users)
                 .setStatus("Đã gửi");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        int day = localDateTime.getDayOfMonth();
+        int month = localDateTime.getMonthValue();
+        int year = localDateTime.getYear();
+        int hour = localDateTime.getHour();
+        int minute = localDateTime.getMinute();
 
+        LocalDateTime dateTime = LocalDateTime.of(year, month, day, hour, minute);
+        recruitmentPlan.setDateCreatePlan(dateTime);
         recruitmentPlan = recruitmentPlanRepository.save(recruitmentPlan);
 
         for (RecruitmentPlanDetail recruitmentPlanDetail : recruitmentPlanDetails) {
