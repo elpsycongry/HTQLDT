61d9ee0f6acfeed79e2e9778806729c8a86ec667
thamuz24 <nguyenhoanggiaminh24@gmail.com>
2024-05-21 08:09:08 +0700

    pull

--- src/main/java/com/example/quanlydaotao/repository/IRecruitmentPlanRepository.java
+++ src/main/java/com/example/quanlydaotao/repository/IRecruitmentPlanRepository.java
@@ -3,11 +3,15 @@ package com.example.quanlydaotao.repository;
 import com.example.quanlydaotao.model.RecruitmentPlan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface IRecruitmentPlanRepository extends JpaRepository<RecruitmentPlan, Long> , JpaSpecificationExecutor<RecruitmentPlan> {
-    Page<RecruitmentPlan> findAll(Pageable pageable);
+public interface IRecruitmentPlanRepository extends JpaRepository<RecruitmentPlan, Long>, JpaSpecificationExecutor<RecruitmentPlan> {
+    List<RecruitmentPlan> findAll(Sort sort);
+
 }
